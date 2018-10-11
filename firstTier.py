d883be61edc80e1abdddd3bfdcf4f2468515518b
krissnnaa <krissnnaa1988@gmail.com>
2018-10-11 17:08:56 -0400

    External artifacts
    Removing itself and python standard libraries to get external artifacts of the IPython.

--- firstTier.py
+++ firstTier.py
@@ -41,5 +41,50 @@ for item in firstTierList:
         wordSplit=item.split('.')[0]
         finalList.append(wordSplit)
 finalList=list(set(finalList))
+
+# Remove itself packages
+temp = []
+itselfList = []
+start = ('.','%','IPython', 'ipython')
+for item in finalList:
+    if item.startswith(start):
+        itselfList.append(item)
+    else:
+        temp.append(item)
+finalList=temp.copy()
+
+temp1=[]
+for item in finalList:
+    if item.endswith('\n') or item.endswith(','):
+        item = item[:-1]
+        temp1.append(item)
+    else:
+        temp1.append(item)
+
+newfinalList = temp1.copy()
+newfinalList = list(set(newfinalList))
+
+# Remove standard libraires to get external libraries
+standardList = []
+externalList = []
+externalList = newfinalList.copy()
+externalList.pop(0)
+with open('standardLibraries.txt') as fs:
+    standardList = fs.read().splitlines()
+
+checkChar = '.'
+finalStandardList = []
+for item in standardList:
+    flag = 0
+    for charecter in item:
+        if charecter == checkChar:
+            flag = 1
+            break
+    if flag == 0:
+        finalStandardList.append(item)
+    else:
+        wordSplit = item.split('.')[0]
+        finalStandardList.append(wordSplit)
+finalStandardList = list(set(finalStandardList))
+finalExternalList=[item for item in externalList if item not in finalStandardList]
         
-    
